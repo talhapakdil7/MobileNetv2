b777d257d57c8a7232727620b2547ca1ccbb1101
Talha Pakdil <talhapakdil7@gmail.com>
2025-10-19 15:00:14 +0300

    Initial Commit

---
+++ MobileNetV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileNetV2
+//
+//  Created by Talha Pakdil on 19.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
